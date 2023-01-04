ced7236d5df6c080c7f290e5c9fb281312fef299
Federico González <federico1818@gmail.com>
2023-01-04 00:57:21 -0300

    subscribe in header to show length

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { StoreService } from 'src/app/services/store.service'
+import { Product } from 'src/app/product'
 
 @Component({
     selector: 'app-header',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
     public shoppingCartLength: number = 0
+
+    constructor(
+        private storeService: StoreService
+    ) {}
+
+    public ngOnInit(): void {
+        this.storeService.cart$.subscribe((products: Product[]) => {
+            this.shoppingCartLength = products.length
+        })
+    }
 }
