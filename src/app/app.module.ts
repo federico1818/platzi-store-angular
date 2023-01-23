32adda4931a4de873c84e7899026b67f397c772e
federico1818 <federico1818@gmail.com>
2023-01-23 15:48:02 +0000

    interceptor to count http time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { SwiperModule } from 'swiper/angular'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component';
-import { ImgComponent } from './img/img.component';
-import { ProductComponent } from './product/product.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
-import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component'
+import { ImgComponent } from './img/img.component'
+import { ProductComponent } from './product/product.component'
+import { CategoriesComponent } from './categories/categories.component'
+import { ProductListComponent } from './product-list/product-list.component'
+import { ReversePipe } from './pipes/reverse.pipe'
+import { HighlightDirective } from './directives/highlight.directive'
+import { HeaderComponent } from './header/header.component'
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { AuthComponent } from './auth/auth.component'
+import { TimeInterceptor } from './interceptors/time.interceptor'
 
 @NgModule({
     declarations: [
@@ -39,7 +40,13 @@ import { AuthComponent } from './auth/auth.component'
 
         AppRoutingModule
     ],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TimeInterceptor,
+            multi: true
+        }
+    ],
     bootstrap: [AppComponent]
 })
 
---
+++ src/app/interceptors/time.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core'
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor
+} from '@angular/common/http'
+import { Observable, tap } from 'rxjs'
+
+@Injectable()
+export class TimeInterceptor implements HttpInterceptor {
+
+    constructor() {}
+
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        const start: number = performance.now()
+        return next.handle(request).pipe(
+            tap(_ => {
+                const time = (performance.now() - start).toFixed(1)
+                console.log(request.url, `${ time } ms`)
+            })
+        )
+    }
+}
